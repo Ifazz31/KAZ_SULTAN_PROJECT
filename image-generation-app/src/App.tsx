3a57c231ef4d596dee675562333f66d96bfefb90
rrijvy <rrijvy@gmail.com>
2024-11-14 15:30:35 +0600

    fix

--- image-generation-app/src/App.tsx
+++ image-generation-app/src/App.tsx
@@ -1,46 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ImageGenerationForm from "./ImageGenerationForm";
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const socket = new WebSocket("ws://127.0.0.1:8188/ws");
-    console.log("check connectection", socket);
-
-    socket.onopen = () => {
-      console.log("WebSocket connection established.");
-    };
-
-    socket.onmessage = (event) => {
-      console.log("data", event.data);
-
-      // try {
-      //   const message = JSON.parse(event.data);
-      //   console.log("WebSocket message:", message);
-
-      //   if (
-      //     message.type === "progress" &&
-      //     message.data &&
-      //     typeof message.data.progress === "number"
-      //   ) {
-      //     setProgress(message.data.progress);
-      //   }
-      // } catch (error) {
-      //   console.error("Error parsing WebSocket message:", error);
-      // }
-    };
-
-    socket.onclose = () => {
-      console.log("Disconnected");
-    };
-
-    socket.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-
-    return () => {
-      socket.close();
-    };
-  }, []);
+  
   return (
     <div>
       <ImageGenerationForm />
--- image-generation-app/src/ImageGenerationForm.tsx
+++ image-generation-app/src/ImageGenerationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const ImageGenerationForm: React.FC = () => {
@@ -13,14 +13,42 @@ const ImageGenerationForm: React.FC = () => {
   const [progress, setProgress] = useState<number>(0);
 
   const sizes = ["256x256", "512x512", "1024x1024"];
-  const schedulers = [
-    "normal",
-    "karras",
-    "exponential",
-    "sgm_uniform",
-    "simple",
-    "beta",
-  ];
+  const schedulers = ["normal", "karras", "exponential", "sgm_uniform", "simple", "beta"];
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://127.0.0.1:8188/ws?clientId=87ee60af06b645d68266e383d95763f4");
+    console.log("check connectection", socket);
+
+    socket.onopen = () => {
+      console.log("WebSocket connection established.");
+    };
+
+    socket.onmessage = (event) => {
+      console.log("data", event.data);
+
+      try {
+        const message = JSON.parse(event.data);
+        if (message.data && message.data.value) {
+          setProgress(message.data.value);
+        }
+        console.log("WebSocket message:", message);
+      } catch (error) {
+        console.error("Error parsing WebSocket message:", error);
+      }
+    };
+
+    socket.onclose = () => {
+      console.log("Disconnected");
+    };
+
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    // return () => {
+    //   socket.close();
+    // };
+  }, []);
 
   const handleGenerate = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -59,7 +87,7 @@ const ImageGenerationForm: React.FC = () => {
   };
 
   const connectWebSocket = () => {
-    const socket = new WebSocket("ws://127.0.0.1:8188/ws?clientId=0af35dd0-e229-46e6-b322-6b10f44bf105");
+    const socket = new WebSocket("ws://127.0.0.1:8188/ws?clientId=87ee60af06b645d68266e383d95763f4");
     console.log("check connectection", socket);
 
     socket.onopen = () => {
@@ -69,16 +97,14 @@ const ImageGenerationForm: React.FC = () => {
     socket.onmessage = (event) => {
       try {
         const message = JSON.parse(event.data);
-    
+
         if (message.type === "status") {
           console.log("Status update:", message.data.status);
-    
         } else if (message.type === "executing") {
           console.log("Currently executing node:", message.data.node);
-    
         } else if (message.type === "progress") {
           if (message.data && message.data.progress) {
-            setProgress(message.data.progress); 
+            setProgress(message.data.progress);
           }
         } else {
           console.warn("Unknown message type:", message.type);
@@ -99,14 +125,9 @@ const ImageGenerationForm: React.FC = () => {
 
   return (
     <div className="container mx-auto flex flex-col items-center h-screen w-full bg-gray-50 p-7">
-      <h1 className="text-center text-4xl font-bold mb-8">
-        Sultan Image Generator
-      </h1>
+      <h1 className="text-center text-4xl font-bold mb-8">Sultan Image Generator</h1>
       <div className="grid gap-8 lg:grid-cols-2 w-full max-w-full flex-grow">
-        <form
-          className="bg-white p-5 rounded-lg shadow-md flex-grow"
-          onSubmit={handleGenerate}
-        >
+        <form className="bg-white p-5 rounded-lg shadow-md flex-grow" onSubmit={handleGenerate}>
           <h2 className="text-3xl font-semibold mb-8">Input</h2>
 
           <label className="block mb-4">
@@ -122,9 +143,7 @@ const ImageGenerationForm: React.FC = () => {
           </label>
 
           <label className="block mb-4">
-            <span className="text-sm font-medium text-gray-700">
-              Negative Prompt
-            </span>
+            <span className="text-sm font-medium text-gray-700">Negative Prompt</span>
             <input
               type="text"
               value={negativePrompt}
@@ -132,9 +151,7 @@ const ImageGenerationForm: React.FC = () => {
               placeholder="e.g., blurry, low resolution"
               className="w-full mt-1 p-3 border border-gray-300 rounded"
             />
-            <small className="text-gray-500">
-              Specify things to avoid in the output image
-            </small>
+            <small className="text-gray-500">Specify things to avoid in the output image</small>
           </label>
 
           <label className="block mb-4">
@@ -142,9 +159,7 @@ const ImageGenerationForm: React.FC = () => {
             <input
               type="number"
               value={samplerSeed ?? ""}
-              onChange={(e) =>
-                setSamplerSeed(e.target.value ? Number(e.target.value) : null)
-              }
+              onChange={(e) => setSamplerSeed(e.target.value ? Number(e.target.value) : null)}
               placeholder="Randomizes output when blank"
               className="w-full mt-1 p-2 border rounded"
             />
@@ -160,15 +175,11 @@ const ImageGenerationForm: React.FC = () => {
               max="50"
               className="w-full mt-1 p-2 border rounded"
             />
-            <small className="text-gray-500">
-              Controls the level of detail in the output
-            </small>
+            <small className="text-gray-500">Controls the level of detail in the output</small>
           </label>
 
           <label className="block mb-4">
-            <span className="text-sm font-medium text-gray-700">
-              CFG (Guidance Scale)
-            </span>
+            <span className="text-sm font-medium text-gray-700">CFG (Guidance Scale)</span>
             <input
               type="number"
               value={samplerCfg}
@@ -177,18 +188,12 @@ const ImageGenerationForm: React.FC = () => {
               max="20"
               className="w-full mt-1 p-2 border rounded"
             />
-            <small className="text-gray-500">
-              Adjusts the influence of the prompt on the output
-            </small>
+            <small className="text-gray-500">Adjusts the influence of the prompt on the output</small>
           </label>
 
           <label className="block mb-4">
             <span className="text-sm font-medium text-gray-700">Size</span>
-            <select
-              value={selectedSize}
-              onChange={(e) => setSelectedSize(e.target.value)}
-              className="w-full mt-1 p-2 border rounded"
-            >
+            <select value={selectedSize} onChange={(e) => setSelectedSize(e.target.value)} className="w-full mt-1 p-2 border rounded">
               {sizes.map((size) => (
                 <option key={size} value={size}>
                   {size}
@@ -224,39 +229,24 @@ const ImageGenerationForm: React.FC = () => {
           <h2 className="text-xl font-semibold mt-32 mb-12">Output</h2>
 
           {outputImage ? (
-            <img
-              src={outputImage}
-              alt="Generated Output"
-              className="w-full rounded-md mt-4"
-            />
+            <img src={outputImage} alt="Generated Output" className="w-full rounded-md mt-4" />
           ) : (
             <p className="text-gray-500">No image generated yet.</p>
           )}
 
           <div className="output-actions flex justify-around mt-4">
-            <button
-              className="p-2 bg-gray-300 rounded-md"
-              disabled={!outputImage}
-            >
+            <button className="p-2 bg-gray-300 rounded-md" disabled={!outputImage}>
               Share
             </button>
-            <button
-              className="p-2 bg-gray-300 rounded-md"
-              disabled={!outputImage}
-            >
+            <button className="p-2 bg-gray-300 rounded-md" disabled={!outputImage}>
               Download
             </button>
           </div>
           {/* Progress Bar */}
           <div className="w-full bg-gray-200 rounded-full h-8 mt-10">
-            <div
-              className="bg-gradient-to-r from-blue-400 to-blue-600 h-full rounded-full"
-              style={{ width: `${progress}%` }}
-            />
+            <div className="bg-gradient-to-r from-blue-400 to-blue-600 h-full rounded-full" style={{ width: `${progress}%` }} />
           </div>
-          {progress > 0 && (
-            <p className="text-center text-sm mt-2">{`Progress: ${progress}%`}</p>
-          )}
+          {progress > 0 && <p className="text-center text-sm mt-2">{`Progress: ${progress}%`}</p>}
         </div>
       </div>
     </div>
